b90a91a7e54d59e9251a78acb4d3995076d9e92a
Jesther Costinar <jesther.jc15@gmail.com>
2023-06-19 10:32:56 +0800

    first commit

---
+++ src/main/java/com/jesthercostinar/joblisting/JobListingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jesthercostinar.joblisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobListingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobListingManagementSystemApplication.class, args);
+	}
+
+}
